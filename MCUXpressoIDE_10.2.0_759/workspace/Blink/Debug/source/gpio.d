e2d91a628656895d33212ad82e763eb49e316e93
Agusgal <agaldeman@yahoo.coma.r>
2020-09-13 18:44:19 -0300

    push del proyecto completo

---
+++ MCUXpressoIDE_10.2.0_759/workspace/Blink/Debug/source/gpio.d
@@ -0,0 +1,21 @@
+source/gpio.o source/gpio.d: ../source/gpio.c ../SDK/CMSIS/MK64F12.h \
+ ../SDK/CMSIS/core_cm4.h ../SDK/CMSIS/cmsis_version.h \
+ ../SDK/CMSIS/cmsis_compiler.h ../SDK/CMSIS/cmsis_gcc.h \
+ ../SDK/CMSIS/system_MK64F12.h ../SDK/CMSIS/MK64F12_features.h \
+ ../source/gpio.h
+
+../SDK/CMSIS/MK64F12.h:
+
+../SDK/CMSIS/core_cm4.h:
+
+../SDK/CMSIS/cmsis_version.h:
+
+../SDK/CMSIS/cmsis_compiler.h:
+
+../SDK/CMSIS/cmsis_gcc.h:
+
+../SDK/CMSIS/system_MK64F12.h:
+
+../SDK/CMSIS/MK64F12_features.h:
+
+../source/gpio.h:
