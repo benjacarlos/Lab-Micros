4b00594958250277921b5f5f76968f5fb74099c3
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2020-10-21 12:45:21 -0300

    Basura cambiada por compilar

--- Entrega FINAL/Debug/source/EncoderHandler.d
+++ Entrega FINAL/Debug/source/EncoderHandler.d
@@ -1,7 +1,11 @@
 source/EncoderHandler.o source/EncoderHandler.d: \
  ../source/EncoderHandler.c ../source/EncoderHandler.h \
- ../source/encoderEvent.h
+ ../source/encoder.h ../source/gpio.h ../source/AdminID.h
 
 ../source/EncoderHandler.h:
 
-../source/encoderEvent.h:
+../source/encoder.h:
+
+../source/gpio.h:
+
+../source/AdminID.h:
