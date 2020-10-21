41bc49bef7ced8dcc9fe021d361ef67a6bdc8111
JuanMartinLaguinge <jlaguinge@itba.edu.ar>
2020-10-21 12:40:17 -0300

    Funciones que se encargan de devolver un char con el numero recibido del encoder

--- Entrega FINAL/source/EncoderHandler.h
+++ Entrega FINAL/source/EncoderHandler.h
@@ -8,15 +8,12 @@
 #ifndef ENCODERHANDLER_H_
 	#define ENCODERHANDLER_H_
 	#include <stdbool.h>
-	//Limites
-	#define MAX_NUM '9'
-	#define MIN_NUM '0'
 	//Flags
 	bool IsPin=false;
 	bool IsID=false;
-	//Funciones
-	void UpdateNumber(void);
-	char GetNumber(void);
-	void ResetNumber(void);
+	//Interfaz
+	void InitNumber(void);
+	bool GettingTotalNumber(void);
+	char* GetTotalNum(void);
 
 #endif /* ENCODERHANDLER_H_ */
