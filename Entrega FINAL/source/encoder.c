65082ea3a49f68ad00e9fc1d540ab0fed8e74fa2
paulonavarro <pnavarro@itba.edu.ar>
2020-10-21 01:16:17 -0300

    L

---
+++ Entrega FINAL/source/encoder.c
@@ -0,0 +1,175 @@
+/*
+ * encoder.c
+ *
+ *  Created on: 14 oct. 2020
+ *  GRUPO 5
+ */
+
+/*******************************************************************************
+ * INCLUIR HEADER
+ ******************************************************************************/
+#include "encoder.h"
+#include "encoderEvent.h"
+#include "encoderHal.h"
+
+
+/******************************************************************************
+ *									DEFINICIONES
+ ******************************************************************************/
+
+#define ENCODER_EVENTS	100
+// VALOR/1000 es los segundos de las señales
+#define BACK_COUNT		200			// entre .5 y 2 segundos para que sea evento = BACK
+#define ENTER_COUNT		5
+
+
+/*******************************************************************************
+ * 								VARIABLES
+ ******************************************************************************/
+_Bool encoderInitialized = falso ;
+static encoderQueue_t encoderQueue[ENCODER_EVENTS];
+
+
+/*******************************************************************************
+ * 								FUNCIONES
+ ******************************************************************************/
+
+
+//	Inicio el encoder
+void initEncoder()
+{
+	if(!encoderInitialized)
+	{
+		initEncoderHAL(rotationCallback);		//setea gpio y timer count
+		setButtonCallback(buttonCallback);
+		initEncoderQueue();			//inicializo queue de encoder
+
+		// inicializo 2 encoder_t con las señales en el instante actual y el anterior (A y B)
+		int n;
+		int k;
+		for( n=0 ; n<STATES ; n++ )					// son 2 encoder_t
+			for( k=0 ; k<CANT_TOTAL_SEÑALES ; k++ )	// reciben las señales A ,B y C
+				updateData(readEncoder(k), k);
+		// se inicializo el encoder
+		initialized = true;
+	}
+}
+
+
+
+
+encoderUd_t pullEncoderEvent(void)
+{
+	encoderUd_t popEvent;
+	if(encoderQueue->top == -1)
+	{
+		encoderQueue->isEmpty = true;
+		popEvent.isValid = false;
+	}
+	else
+	{
+		popEvent = encoderQueue[encoderQueue->top].event; //popEvent
+		popEvent.isValid = true;
+		encoderQueue->top -= 1; // Decrement queue counter
+		if(encoderQueue->top == -1)
+		{
+			encoderQueue->isEmpty = true;
+		}
+	}
+	return popEvent;
+}
+
+void pushEncoderEvent(encoderUd_t newEvent){
+	if(encoderQueue->top == ENCODER_EVENTS-1)
+	{ // event overflow
+		encoderQueue->top = 0;
+		encoderQueue[encoderQueue->top].event = newEvent;
+		encoderQueue->isEmpty = false;
+	}
+	else{
+		encoderQueue->top += 1;
+		encoderQueue[encoderQueue->top].event = newEvent;
+		encoderQueue->isEmpty = false;
+	}
+	return;
+}
+
+
+
+//	Verifico si la cola de eventos del encoder esta vacía
+_Bool isEncoderEventValid(void)
+{
+	return !encoderQueue->isEmpty;
+}
+
+
+
+void rotationCallback(void)
+{
+	updateData(readEncoder(A), A);
+	updateData(readEncoder(B), B);
+	encoderUd_t eventEncoderQueue;
+	eventEncoderQueue.isValid = true;
+	counter_type event = encoderRot();
+
+
+	if(event == COUNT_UP)
+	{
+		eventEncoderQueue.input = UP;
+		pushEncoderEvent(eventEncoderQueue);
+		resetData();
+	}
+	else if(event == COUNT_DOWN)
+	{
+		eventEncoderQueue.input = DOWN;
+		pushEncoderEvent(eventEncoderQueue);
+		resetData();
+	}
+	else
+	{
+		eventEncoderQueue.isValid = false;
+		resetData();
+
+	}
+
+}
+
+
+void initEncoderQueue(void)
+{
+	encoderQueue->top = -1;
+	encoderQueue->isEmpty = true;
+}
+
+
+
+void buttonCallback(void)
+{
+	updateData(readEncoder(C), C);
+
+	encoderQueue_t eventEncoderQueue;
+ 	eventEncoderQueue.event.isValid = true;
+	if(checkFallingEdge())							//si fue flanco descendente -->> se presionó el botón
+	{
+		resetEncoderTimerCount();					//reseteo el contador
+	}
+	else if(checkRisingEdge())						//si fue un flanco ascendente me fijo cuánto tiempo se presionó el botón para saber si fue ENTER o BACK
+	{
+		if(getEncTimerCount() <= ENTER_COUNT)		//si es menor a ENTER_COUNT el evento es ENTER
+		{
+			eventEncoderQueue.event.input = ENTER;
+			eventEncoderQueue.event.isValid = true;
+			pushEncoderEvent(eventEncoderQueue.event);
+		}
+		else 										//si es menos de BACK_COUNT o mas de ENTER_COUNT el evento es BACK
+		{
+			eventEncoderQueue.event.input = BACK;
+			eventEncoderQueue.event.isValid = true;
+			pushEncoderEvent(eventEncoderQueue.event);
+		}
+	}
+}
+
+
+
+
---
+++ Entrega FINAL/source/encoder.h
@@ -0,0 +1,56 @@
+/*
+ * encoder.h
+ * Created on: 14 oct. 2020
+ * GRUPO 5
+ */
+
+#ifndef SOURCES_ENCODER_H_
+#define SOURCES_ENCODER_H_
+
+
+/*******************************************************************************
+ * INCLUIR HEADER
+ ******************************************************************************/
+
+#include "gpio.h"
+#include <stdbool.h>
+
+/*******************************************************************************
+ * 	DEFICIONES
+ ******************************************************************************/
+
+
+#define LOW 	0
+#define HIGH	1
+
+typedef enum { UP , DOWN , ENTER , BACK }encoder_type;	//
+
+// valores que toma el evento
+typedef struct{
+	encoder_type input;
+	_Bool isValid;
+}encoderUd_t;
+
+// cola de eventos
+typedef struct{
+	int top; 			//contador de eventos
+	encoderUd_t event;
+	_Bool isEmpty;
+}encoderQueue_t;
+
+/*******************************************************************************
+ * FUNCIONES
+ ******************************************************************************/
+
+void initEncoder(void);					// inicio el encoder en general
+//
+encoderUd_t pullEncoderEvent(void);		// saco el ultimo evento de la cola de evento
+void pushEncoderEvent(encoderUd_t event);	// meto un evento en la cola de evento
+_Bool isEncoderEventValid(void);		//
+void rotationCallback(void);			//
+void initEncoderQueue(void);			// inicio la cola de evento del encoder
+
+//encoderQueue_t getEncoderQueue(void);  // por ahora no hace falta tomar eventos
+void buttonCallback(void);
+
+#endif /* SOURCES_ENCODER_H_ */
