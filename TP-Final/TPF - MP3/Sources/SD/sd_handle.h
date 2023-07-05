94add9f64c2de06cd21020be0a901127280db8bb
Benjamin Lin <blin@itba.edu.ar>
2023-07-05 20:09:20 -0300

    SD Module

---
+++ TP-Final/TPF - MP3/Sources/SD/sd_handle.h
@@ -0,0 +1,54 @@
+/*
+ * SD.h
+ *
+ *  Created on: 5 Jul 2023
+ *      Author: BZeroL
+ */
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+#include <stdio.h>
+#include "fsl_sd.h"
+
+
+#ifndef SOURCES_SD_SD_HANDLE_H_
+#define SOURCES_SD_SD_HANDLE_H_
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+#define SD_BUFFER_ALIGN_SIZE 	(4U)
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*
+ * Init routine
+ * cd: a callback to receive and store the scanned files
+ */
+void SDHandle_Init(void);
+
+/*
+ * SD present or not
+ */
+bool SDHandle_IsSDPresent(void);
+
+/*
+ *	SD initialization routine
+ */
+void SDHandle_OnConnection(void);
+
+/*
+ * SD deinit routine
+ */
+void SDHandle_OnDesconection(void);
+
+bool SDHandle_SDConnection(void);
+
+bool SDHandle_SDDesconnection(void);
+
+
+
+#endif /* SOURCES_SD_SD_HANDLE_H_ */
