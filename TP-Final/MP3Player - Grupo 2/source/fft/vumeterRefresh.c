de68d29b93631a33542bc7d94bcb7b4f2beaf896
JuanMartinLaguinge <42174219+JuanMartinLaguinge@users.noreply.github.com>
2023-08-05 16:50:41 -0300

    vumeter cleaning

--- TP-Final/MP3Player - Grupo 2/source/fft/vumeterRefresh.c
+++ TP-Final/MP3Player - Grupo 2/source/fft/vumeterRefresh.c
@@ -1,3 +1,12 @@
+/*****************************************************************************
+  @file     vumeterRefresh.c
+  @author   Grupo 5
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
 #include "arm_math.h"
 #include "math.h"
 #include "matrix_display.h"
@@ -6,19 +15,36 @@
 #include "vumeterRefresh.h"
 #include "config_main.h"
 
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
 
 #define SAMPLE_LENGTH       FFT_SIZE
+// Width and height of frequency bands that hardware can display
 #define NUMBER_OF_BANDS     8  
 #define VUMETER_HEIGHT      8
+// Value of vumeter amplitude consider noise and max amplitude possible between VUMETER_HEIGHT
 #define NOISE               5
 #define MAX_AMPLITUDE       50
+
 #define AVERAGE				2
+
+/*******************************************************************************
+ * PRIVATE VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
 
 static arm_rfft_fast_instance_f32 rfft_fast_instance;
 static float32_t output[SAMPLE_LENGTH];
+// Where we store the needed half of the frequency spectrum
 static float32_t outputFft[SAMPLE_LENGTH / 2];
+// Vumeter bins matrix
 static int vumeterMatrix[NUMBER_OF_BANDS];
+// vumeter display matrix
 static colors_t auxMatrix[VUMETER_HEIGHT * NUMBER_OF_BANDS];
+
+/*******************************************************************************
+ *                        GLOBAL FUNCTION DEFINITIONS
+ ******************************************************************************/
 
 void vumeterRefresh_init()
 {
@@ -30,12 +56,13 @@ void vumeterRefresh_init()
 int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, int lowerFreqBand, int higherFreqBand)
 { 
     static volatile char average = 0;
-    volatile unsigned int usableBins = (SAMPLE_LENGTH / 2 - 1);
 
+    volatile unsigned int usableBins = (SAMPLE_LENGTH / 2 - 1);
+    // Variables for scaling spectrum to vumeter display bins resolution
     volatile double inv_binWidth = SAMPLE_LENGTH/sampleRate;
     volatile double base = higherFreqBand / lowerFreqBand;
     volatile double exp = 1.0f / (NUMBER_OF_BANDS - 1.0f );
-    volatile float32_t freqMultiplierPerBand = pow(base,exp);
+    volatile float32_t freqMultiplierPerBand = pow(base,exp);	// We use the multiplier (Max/Min)^[1/(Bands-1)] to divide the spectrum range in equal parts
 
     arm_rfft_fast_f32(&rfft_fast_instance, inputSignal, output, 0);
 
@@ -47,21 +74,22 @@ int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, int lowerF
 
     outputFft[0] = 0; //Removing DC
 
-
+    // Variables of bins spectrometer
     volatile float32_t currentBinFreq = lowerFreqBand;
     volatile float32_t nextBinFreq = lowerFreqBand * freqMultiplierPerBand;
     volatile float32_t currentCenterBin;
     volatile float32_t nextCenterBin;
-    volatile float32_t lowerBin = 0;
-    volatile float32_t higherBin;
+    volatile float32_t lowerBin = 0;	// Starting FFT bin for the currentBinFreq range
+    volatile float32_t higherBin;		// Ending FFT bin for the currentBinFreq range
     volatile float32_t temp;
+    // Calculate bins values
     for (size_t i = 0; i < NUMBER_OF_BANDS; i++)
     {
 
         currentCenterBin = currentBinFreq * inv_binWidth;
         nextCenterBin = nextBinFreq * inv_binWidth;
         lowerBin = i ? higherBin : 0;
-        higherBin = (nextCenterBin-currentCenterBin)*0.5 + currentCenterBin;//calcular higherBin!!!
+        higherBin = (nextCenterBin-currentCenterBin)*0.5 + currentCenterBin;
 
         temp = 0;
         for (size_t j = (int)(lowerBin); ((j < (int)(higherBin))&&(j < usableBins)); j++)
@@ -69,7 +97,7 @@ int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, int lowerF
         	temp += (outputFft[j] > NOISE)? outputFft[j] : 0;
         }
         int roundedHeight = (int)(temp/MAX_AMPLITUDE);
-        //int roundedHeight = floor((vumeterValues[i]/(higherBin - lowerBin))/1000);
+
         vumeterMatrix[i] += (roundedHeight > VUMETER_HEIGHT ? VUMETER_HEIGHT : roundedHeight)/AVERAGE;
 
         currentBinFreq = nextBinFreq;
--- TP-Final/MP3Player - Grupo 2/source/fft/vumeterRefresh.h
+++ TP-Final/MP3Player - Grupo 2/source/fft/vumeterRefresh.h
@@ -1,10 +1,28 @@
+/*****************************************************************************
+  @file     vumeterRefresh.h
+  @author   Grupo 5
+ ******************************************************************************/
+
 #ifndef _VUMETER_REFRESH_H
 #define _VUMETER_REFRESH_H
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
 #include<stdio.h>
 #include<stdint.h>
 #include "arm_math.h"
 
-#define FFT_SIZE	1024
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define FFT_SIZE	1024	// Number of sample use to get good spectral resolution
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
 
 void vumeterRefresh_init();
 int vumeterRefresh_fft(float32_t * inputSignal, float32_t sampleRate, int lowerFreqBand, int higherFreqBand);
@@ -13,7 +31,3 @@ void vumeterRefresh_draw_display();
 void vumeterRefresh_clean_display();
 
 #endif
-/**
- *
- * End of file.
- */
